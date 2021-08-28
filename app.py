134f6af01187899f8fec096b6ddaf28a42601ac7
Matt <mattrager@gmail.com>
2021-08-27 21:55:03 -0600

    final product

--- app.py
+++ app.py
@@ -1,31 +1,10 @@
 """Spotify predicted playlist based on user inputs."""
-import os
-import pandas as pd
-import json
 import spotipy
-from flask import Flask, request, render_template, json, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, render_template
 from functions import find_neighbors, search_song_data
 
 
-DB = SQLAlchemy()
 api = spotipy.Spotify()
-
-
-class Spotify(DB.Model):
-    """Set Flask class values for results."""
-
-    id = DB.Column(DB.Integer, primary_key=True, nullable=False)
-    genre = DB.Column(DB.String, nullable=False)
-    sub_genre = DB.Column(DB.String, nullable=False)
-    artist = DB.Column(DB.String, nullable=False)
-    album = DB.Column(DB.String, nullable=False)
-    title = DB.Column(DB.String, nullable=False)
-    tempo = DB.Column(DB.Float, nullable=False)
-    length = DB.Column(DB.Integer, nullable=False)
-
-    def __repr__(self):
-        return f"[ID: {self.id} | Genre: {self.genre} | Sub-Genre: {self.sub_genre} | Artist: {self.artist} | Album: {self.album} | Title: {self.title} | Tempo: {self.tempo} | Length: {self.length}]"
 
 
 def create_app():
@@ -48,33 +27,6 @@ def create_app():
 
 
         return render_template('deep_landing.html')
-
-
-
-
-
-    @app.route('/reset')
-    def reset_db():
-        """Reset the database"""
-        DB.drop_all()
-        DB.create_all()
-        return render_template('deep_reset.html')
-
-    @app.route('/data', methods=['GET', 'POST'])
-    def data():
-        """Create a page view with all data from the original dataset"""
-        df = pd.read_csv('C:\\Users\\mattr\\Repositories\\Unit3BuildWeek\\Modelling\\Data\\new_songs_cleaned.csv')
-        result = df.to_json(orient="index")
-        parsed = json.loads(result)
-        return json.dumps(parsed, indent=4)
-
-    @app.route('/results', methods=['GET', 'POST'])
-    def prediction_results():
-        """Results page view showing predictions"""
-        results = find_neighbors(prediction_results)
-        if request.method == 'POST':
-            return render_template('deep_results.html', results=results)
-        return render_template('deep_results.html')
 
     return app
 
